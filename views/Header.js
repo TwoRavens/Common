e0a1affe16034eea9130e443ef7c6fa9b7946bea
Shoeboxam <shoeboxam@gmail.com>
2018-05-25 17:46:09 -0500

    header vertical alignment

--- views/Header.js
+++ views/Header.js
@@ -23,20 +23,28 @@ export default class Header {
     view(vnode) {
         let {image, attrsInterface} = vnode.attrs;
 
+        // navbar-default is for bootstrap 3 compatibility
         return m('nav.navbar.navbar-expand-lg.fixed-top.bg-light.navbar-default', mergeAttributes(
-            {style: {'min-height': heightHeader, 'box-shadow': '0 0 4px #888'}}, attrsInterface), [
-            m("a.navbar-brand",
-                m("img[alt=TwoRavens][width=100][style=margin-left: 1em]", {
-                    onmouseover: _ => this.about = true,
-                    onmouseout: _ => this.about = false,
-                    src: image
-                })),
-            m(`#about.card[style=display: ${this.about ? 'block' : 'none'}; top: 10px; left: 140px; position: absolute; width: 500px; z-index: 50]`,
-                m('.card-body', ABOUT)),
+            {style: {'min-height': heightHeader, 'box-shadow': '0 0 4px #888', 'z-index': 1000, 'height': 'auto'}}, attrsInterface), [
+            m("img.navbar-brand[alt=TwoRavens]", {
+                style: {height:'100%', 'max-height': heightHeader, 'max-width': '140px'},
+                onmouseover: _ => this.about = true,
+                onmouseout: _ => this.about = false,
+                src: image
+            }),
+            m(`#about.about[style=display: ${this.about ? 'block' : 'none'}; top: 10px; left: 140px; position: absolute; width: 500px; z-index: 50]`,
+                ABOUT),
 
+            // This styling is partially and conditionally overwritten via common.css @media queries for mobile formatting
             m('div#hamburger.show-mobile', {
                 onclick: () => this.mobileHeader = !this.mobileHeader,
-                style: {display: 'none', float: 'right'}
+                style: {
+                    display: 'none',
+                    float: 'right',
+                    'margin-top': `calc(${heightHeader} / 2)`,
+                    'margin-bottom': `calc(-${heightHeader} / 2)`,
+                    transform: 'translateY(-50%)'
+                }
             }, m('div.header-icon', {
                 style: {
                     transform: 'scale(1.75, 1.5)',
@@ -46,7 +54,14 @@ export default class Header {
 
             m('div#headerMenu', {
                     class: !this.mobileHeader && ['hide-mobile'],
-                    style: {width: 'calc(100% - 136px)'}
+                    style: {
+                        width: 'calc(100% - 140px)',
+                        display: 'inline-grid',
+                        float: 'right',
+                        'margin-top': `calc(${heightHeader} / 2)`,
+                        'margin-bottom': `calc(-${heightHeader} / 2)`,
+                        transform: 'translateY(-50%)'
+                    }
                 },
                 m('div#headerContent', {
                         style: {
