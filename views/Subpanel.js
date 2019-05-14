fad8da84c6ab639f0771888ab341c32f85ac317f
Shoeboxam <shoeboxam@gmail.com>
2019-05-14 16:23:30 -0500

    Subpanel shown and setShown attrs
    allows external configuration of open/closed state

--- views/Subpanel.js
+++ views/Subpanel.js
@@ -15,19 +15,24 @@ import Button from "./Button";
 
 export default class Subpanel {
     oninit() {
-        this.show = true;
+        this.shown = true;
     }
 
     view({attrs, children}) {
-        let {id, header} = attrs;
+        let {id, header, shown, setShown} = attrs;
+
+        setShown = setShown || (state => this.shown = state);
+
+        // set state from attrs if defined
+        if (shown !== undefined) this.shown = shown;
 
         return m(`div.card`, mergeAttributes({
                 style: {'margin-bottom': '0px'}
             },
             attrs),
-            m(".card-header", {onclick: () => this.show = !this.show},
+            m(".card-header", {onclick: () => setShown(!this.shown)},
                 m("h4.card-title", {style: {'margin-bottom': '0'}}, header,
-                    m(Icon, {style: 'margin:.25em 0 0 .5em;float:right', name: 'triangle-' + (this.show ? 'down' : 'up')}))),
+                    m(Icon, {style: 'margin:.25em 0 0 .5em;float:right', name: 'triangle-' + (this.shown ? 'down' : 'up')}))),
 
                     // m(`span.glyphicon.glyphicon-large.glyphicon-chevron-${this.show ? 'down' : 'up'}`, {
                     //     style: {float: 'right', 'margin-left': '.5em'},
@@ -36,7 +41,7 @@ export default class Subpanel {
                     //     'href': `#${id}Body`,
                     //     onclick: () => this.show = !this.show
                     // }))),
-            m(`div#${id}Body`, this.show && m('div.card-body', children))
+            m(`div#${id}Body`, this.shown && m('div.card-body', children))
         );
     }
 }
