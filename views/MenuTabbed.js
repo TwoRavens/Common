35e29b95a4731327e412e42a0da0f5214a6ad40e
Shoeboxam <shoeboxam@gmail.com>
2019-07-09 11:10:11 -0700

    MenuTabbed store default state

--- views/MenuTabbed.js
+++ views/MenuTabbed.js
@@ -28,19 +28,24 @@ export default class MenuTabbed {
     view(vnode) {
         let {id, sections, callback, selectWidth, hoverBonus, currentTab, attrsAll} = vnode.attrs;
 
+        this.currentTab = currentTab || this.currentTab;
+        this.callback = callback || (tab => this.currentTab = tab);
+
         // If a button is not visible, then create the element for the DOM anyways-- but don't let it affect the css
         let visibleButtons = [];
         let invisibleButtons = [];
         for (let section of sections) {
             if (!section) continue;
-            if (section['display'] === 'none') invisibleButtons.push(section);
+            if (section.display === 'none') invisibleButtons.push(section);
             else {
-                section['onclick'] = () => callback(section['value']);
+                section.onclick = () => callback(section.value);
                 visibleButtons.push(section);
             }
             // Automatically build the id
-            section['id'] = 'btn' + (section['idSuffix'] || section['value']);
+            section.id = 'btn' + (section.idSuffix || section.value);
         }
+
+        if (sections.length === 0) return;
 
         // Contents to render for the section
         return m('', mergeAttributes( {
@@ -58,7 +63,7 @@ export default class MenuTabbed {
                 sections: visibleButtons,
                 attrsAll: {style: {'margin-bottom': '5px'}},
                 hoverBonus: hoverBonus,
-                activeSection: currentTab,
+                activeSection: this.currentTab,
                 selectWidth: selectWidth
             }),
             m(ButtonRadio, {
@@ -71,9 +76,9 @@ export default class MenuTabbed {
             }),
             sections
                 .filter(section => section) // ignore undefined sections
-                .map(section => m(`div#tab${section['idSuffix'] || section['value']}`, {
+                .map(section => m(`div#tab${section.idSuffix || section.value}`, {
                     style: {
-                        display: section['value'] === currentTab ? 'block' : 'none',
+                        display: section.value === this.currentTab ? 'block' : 'none',
                         overflow: 'auto',
                         flex: 1
                     }
