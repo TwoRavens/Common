cd776bb468f9f1169ae50da1bcddccc62180d18f
Shoeboxam <shoeboxam@gmail.com>
2019-07-31 17:34:22 -0400

    TwoPanel permit arbitrary parent div when resizing

--- views/TwoPanel.js
+++ views/TwoPanel.js
@@ -17,6 +17,36 @@ export default class TwoPanel {
     oninit() {
         this.focus = 'left';
         this.previous = this.focus;
+        this.isResizingMenu = false;
+
+        // window resizing
+        this.leftpanelSize = 50;
+    }
+
+
+    resizeMenu(e, dom) {
+        this.isResizingMenu = true;
+        document.body.classList.add('no-select');
+        this.resizeMenuTick(e, dom);
+    }
+
+    resizeMenuTick(e, dom) {
+        if (!dom) return;
+        this.leftpanelSize = (1 - e.clientX / dom.clientWidth) * 100;
+
+        document.getElementById('leftView').style.right = this.leftpanelSize + "%";
+        document.getElementById('rightView').style.width = this.leftpanelSize + "%";
+    }
+
+    oncreate({dom}) {
+        dom.addEventListener('mousemove', e => this.isResizingMenu && this.resizeMenuTick(e, dom));
+
+        dom.addEventListener('mouseup', () => {
+            if (this.isResizingMenu) {
+                this.isResizingMenu = false;
+                document.body.classList.remove('no-select');
+            }
+        })
     }
 
     view(vnode) {
@@ -25,7 +55,7 @@ export default class TwoPanel {
         let animate = this.focus !== this.previous;
         this.previous = this.focus;
 
-        return [
+        return m('div',
             m('div#leftView', mergeAttributes({
                 onclick: () => this.focus = 'left',
                 class: {
@@ -38,7 +68,7 @@ export default class TwoPanel {
                     left: 0,
                     top: 0,
                     bottom: 0,
-                    right: leftpanelSize + '%',
+                    right: this.leftpanelSize + '%',
                     'overflow-y': 'auto',
                     'animation-duration': '.4s'
                 }
@@ -54,7 +84,7 @@ export default class TwoPanel {
                     right: 0,
                     top: 0,
                     bottom: 0,
-                    width: leftpanelSize + '%',
+                    width: this.leftpanelSize + '%',
                     'overflow-y': 'auto',
                     'animation-duration': '.4s'
                 }
@@ -69,35 +99,10 @@ export default class TwoPanel {
                         width: '12px',
                         cursor: 'w-resize'
                     },
-                    onmousedown: resizeMenu
+                    onmousedown: e => this.resizeMenu(e, vnode.dom)
                 }),
                 right
             ])
-        ]
+        )
     }
 }
-
-// window resizing
-let isResizingMenu = false;
-export let leftpanelSize = 50;
-export let resizeMenu = (e) => {
-    isResizingMenu = true;
-    document.body.classList.add('no-select');
-    resizeMenuTick(e);
-};
-
-let resizeMenuTick = (e) => {
-    leftpanelSize = (1 - e.clientX / document.getElementById('canvas').clientWidth) * 100;
-
-    document.getElementById('leftView').style.right = leftpanelSize + "%";
-    document.getElementById('rightView').style.width = leftpanelSize + "%";
-};
-
-document.onmousemove = (e) => isResizingMenu && resizeMenuTick(e);
-
-document.onmouseup = () => {
-    if (isResizingMenu) {
-        isResizingMenu = false;
-        document.body.classList.remove('no-select');
-    }
-};
